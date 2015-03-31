6160b6336ee80871eae639d26c5377e180abf9b7
mattdesl <dave.des@gmail.com>
2015-03-31 10:07:54 -0400

    skipping live reload script; also adding timeout to fix watchify 3.1.0 issues (will revisit)

--- example/index.js
+++ example/index.js
@@ -7,5 +7,5 @@ function render(ctx, width, height, dt) {
 
     ctx.clearRect(0, 0, width, height)
     var x = Math.sin(time) * 50
-    ctx.fillRect(150 + x, 200, 155, 145)
+    ctx.fillRect(150 + x, 50, 155, 250)
 }
